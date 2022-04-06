8d26307d59278710d086e40dbfc6c5733bc08bba
Phyllice Wai <waikahkwan0903@gmail.com>
2022-04-06 12:14:27 +0800

    Enhancement on getSwapPrice()

--- src/utils/getList.ts
+++ src/utils/getList.ts
@@ -1,11 +1,11 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /** @format */
 
-import {SEOracleAddress, ETHOracleAddress, mintAddress, USDCaddress, SwapFactoryAddress, SwapRouterAddress, LongStakingAddress, oracleList} from 'constants/index'
-import {ethers} from 'ethers'
-import {formatUnits} from 'ethers/lib/utils'
-import {fixD} from 'utils'
-import {getLibrary} from 'utils/getLibrary'
+import { SEOracleAddress, ETHOracleAddress, mintAddress, USDCaddress, SwapFactoryAddress, SwapRouterAddress, LongStakingAddress, oracleList, getLpPairDetail } from 'constants/index'
+import { ethers } from 'ethers'
+import { formatUnits } from 'ethers/lib/utils'
+import { fixD } from 'utils'
+import { getLibrary } from 'utils/getLibrary'
 import MintAbi from 'constants/abis/Mint.json'
 import ETHOracle from 'constants/abis/ETHOracle.json'
 import STAOracle from 'constants/abis/STAOracle.json'
@@ -13,7 +13,7 @@ import Erc20Abi from 'constants/abis/erc20.json'
 import SwapFactoryAbi from 'constants/abis/swapFactory.json'
 import lpContractAbi from 'constants/abis/lpContract.json'
 import store from '../state/index'
-import {simpleRpcProvider} from './providers'
+import { simpleRpcProvider } from './providers'
 
 import {
   upDateAssetBaseInfoObj,
@@ -29,33 +29,35 @@ import {
 import { upDateCoinSelect as upDateMintCoinSelect, upDateCoinStock as upDateMintCoinStock } from 'state/mint/actions'
 import { upDateFarmCoinSelect, upDateCoinStock as upDateFarmCoinStock } from 'state/farm/actions'
 import { upDateTradeCoinSelect, upDateCoinStock as upDateTradeCoinStock } from 'state/trade/actions'
-import { upDatePositionInfo} from 'state/manage/actions'
+import { upDatePositionInfo } from 'state/manage/actions'
 
 export async function getCommonAssetInfo(account?: string) {
   const dispatch = store.dispatch
-  dispatch(upDatePositionInfo({ positionInfo: {
-    assetTokenName: 'nSE',
-    assetToken: '',
-    cAssetToken: '',
-    cAssetTokenName: 'USDC',
-    assetAmount: '',
-    cAssetAmount: '',
-    cRatio: '',
-    isShort: '',
-  } }))
+  dispatch(upDatePositionInfo({
+    positionInfo: {
+      assetTokenName: 'nSE',
+      assetToken: '',
+      cAssetToken: '',
+      cAssetTokenName: 'USDC',
+      assetAmount: '',
+      cAssetAmount: '',
+      cRatio: '',
+      isShort: '',
+    }
+  }))
   dispatch(upDateMintCoinSelect({ mintCoinSelect: '' }))
   dispatch(upDateMintCoinStock({ mintCoinStock: '' }))
   dispatch(upDateFarmCoinSelect({ farmCoinSelect: '' }))
   dispatch(upDateFarmCoinStock({ farmCoinStock: '' }))
   dispatch(upDateTradeCoinSelect({ tradeCoinSelect: '' }))
   dispatch(upDateTradeCoinStock({ tradeCoinStock: '' }))
-  dispatch(upDateOpenWeb({openWeb: false}))
+  dispatch(upDateOpenWeb({ openWeb: false }))
   const provider = window.ethereum
   const library = getLibrary(provider) || simpleRpcProvider
   const assetBaseInfo: any = []
   let assetBaseInfoArr: any = []
   const config = await getAssetList()
-  dispatch(updateDefaultCAsset({defaultCAsset: config.default.cAsset}))
+  dispatch(updateDefaultCAsset({ defaultCAsset: config.default.cAsset }))
   dispatch(updateDefaultAsset({ defaultAsset: config.default.asset }))
   const assetBaseInfoObj: any = config.assetPre
 
@@ -107,11 +109,11 @@ export async function getCommonAssetInfo(account?: string) {
       }
 
       // Price oracle
-      if(assetBaseInfoObj[asset].type === 'asset') {
+      if (assetBaseInfoObj[asset].type === 'asset') {
         const assetName = assetBaseInfoObj[asset].name
         const oracleInfo = oracleList.find(i => i.assetKey === assetName)
 
-        if(oracleInfo !== undefined) {
+        if (oracleInfo !== undefined) {
           const priceOracleContract = new ethers.Contract(oracleInfo.address, STAOracle, library)
           const price = await priceOracleContract.latestRoundData()
           assetBaseInfoObj[asset].oraclePrice = fixD(formatUnits(price.answer, 8), 4)
@@ -126,7 +128,7 @@ export async function getCommonAssetInfo(account?: string) {
       if (swapPriceResult) {
         const token0Name = assetsName[swapPriceResult.token0]
         const token1Name = assetsName[swapPriceResult.token1]
-        
+
         const reserves0 = Number(formatUnits(swapPriceResult.reserves[0], assetBaseInfoObj[token0Name].decimals))
         const reserves1 = Number(formatUnits(swapPriceResult.reserves[1], assetBaseInfoObj[token1Name].decimals))
 
@@ -138,43 +140,44 @@ export async function getCommonAssetInfo(account?: string) {
           } else {
             assetBaseInfoObj[asset].swapPrice = reserves0 / reserves1
           }
-        if(reserves0 > 0 && reserves1 > 0) {
-          if (swapPriceResult.token0 == assetBaseInfoObj[asset].address) {
-            assetBaseInfoObj[asset].swapPrice = reserves1 / reserves0
+          if (reserves0 > 0 && reserves1 > 0) {
+            if (swapPriceResult.token0 == assetBaseInfoObj[asset].address) {
+              assetBaseInfoObj[asset].swapPrice = reserves1 / reserves0
+            } else {
+              assetBaseInfoObj[asset].swapPrice = reserves0 / reserves1
+            }
           } else {
-            assetBaseInfoObj[asset].swapPrice = reserves0 / reserves1
+            // Return 0 if either one of reserves is 0
+            assetBaseInfoObj[asset].swapPrice = 0
           }
-        } else {
-          // Return 0 if either one of reserves is 0
-          assetBaseInfoObj[asset].swapPrice = 0
         }
       }
-    }
 
-    if (assetBaseInfoObj[asset].type == 'asset') {
-      assetsListInfo.push(assetBaseInfoObj[asset])
-    } else {
-      if (assetBaseInfoObj[asset].name !== 'WMATIC') {
-        if (assetBaseInfoObj[asset].name !== 'NSDX') {
-          cAssetsListInfo.push(assetBaseInfoObj[asset])
+      if (assetBaseInfoObj[asset].type == 'asset') {
+        assetsListInfo.push(assetBaseInfoObj[asset])
+      } else {
+        if (assetBaseInfoObj[asset].name !== 'WMATIC') {
+          if (assetBaseInfoObj[asset].name !== 'NSDX') {
+            cAssetsListInfo.push(assetBaseInfoObj[asset])
+          }
         }
       }
+
+      allAssetsListInfo.push(assetBaseInfoObj[asset])
     }
 
-    allAssetsListInfo.push(assetBaseInfoObj[asset])
+    const MintContract = new ethers.Contract(mintAddress, MintAbi, library)
+    const feerate = (await MintContract.feeRate()) / 1000
+    // const SEOracleContract = new ethers.Contract(SEOracleAddress, STAOracle, library)
+    // const SEOraclePrice = await SEOracleContract.latestRoundData()
+    // assetBaseInfoObj['nSE'].oraclePrice = fixD(formatUnits(SEOraclePrice.answer, 8), 4)
+    dispatch(upDateAssetsNameInfo({ assetsNameInfo: assetsName }))
+    dispatch(upDateAssetBaseInfoObj({ assetBaseInfoObj: assetBaseInfoObj }))
+    dispatch(upDateAssetsListInfo({ assetsListInfo: assetsListInfo }))
+    dispatch(upDateCAssetsListInfo({ cAssetsListInfo: cAssetsListInfo }))
+    dispatch(upDateAllAssetsListInfo({ allAssetsListInfo: allAssetsListInfo }))
+    dispatch(upDateCommonFee({ feeRate: feerate * 100 }))
   }
-
-  const MintContract = new ethers.Contract(mintAddress, MintAbi, library)
-  const feerate = (await MintContract.feeRate()) / 1000
-  // const SEOracleContract = new ethers.Contract(SEOracleAddress, STAOracle, library)
-  // const SEOraclePrice = await SEOracleContract.latestRoundData()
-  // assetBaseInfoObj['nSE'].oraclePrice = fixD(formatUnits(SEOraclePrice.answer, 8), 4)
-  dispatch(upDateAssetsNameInfo({assetsNameInfo: assetsName}))
-  dispatch(upDateAssetBaseInfoObj({assetBaseInfoObj: assetBaseInfoObj}))
-  dispatch(upDateAssetsListInfo({assetsListInfo: assetsListInfo}))
-  dispatch(upDateCAssetsListInfo({cAssetsListInfo: cAssetsListInfo}))
-  dispatch(upDateAllAssetsListInfo({allAssetsListInfo: allAssetsListInfo}))
-  dispatch(upDateCommonFee({feeRate: feerate * 100}))
 }
 
 export async function getAssetList(): Promise<any> {
@@ -188,18 +191,34 @@ export async function getAssetList(): Promise<any> {
 }
 
 export async function getSwapPrice(tokenAaddress: any, tokenBaddress: any) {
-  let price: any
-  const provider = window.ethereum
-  const library = getLibrary(provider)??simpleRpcProvider
-  const swapFactoryContract = new ethers.Contract(SwapFactoryAddress, SwapFactoryAbi, library)
-  const result = await swapFactoryContract.getPair(tokenAaddress, tokenBaddress)
-  if (Number(formatUnits(result, 18)) > 0) {
-    const contract = new ethers.Contract(result, lpContractAbi, library)
+  try {
+    if (tokenAaddress === undefined || tokenBaddress === undefined) {
+      throw new Error(`Token A / Token B address is undefined`)
+    }
+    let price: any
+    console.log(`Get swap price at ${new Date().toString()}`)
+    const provider = window.ethereum
+    const library = getLibrary(provider) ?? simpleRpcProvider
+
+    const lpInfo = getLpPairDetail(tokenAaddress, tokenBaddress)
+    if (lpInfo === undefined) {
+      throw new Error(`LP info is undefined`)
+    }
+
+    const lpAddress = lpInfo.lp
+    const contract = new ethers.Contract(lpAddress, lpContractAbi, library)
     const reserves = await contract.getReserves()
-    const token0 = await contract.token0()
-    const token1 = await contract.token1()
-    return {token0, token1, reserves, result}
+
+    return {
+      token0: lpInfo.tokenA,
+      token1: lpInfo.tokenB,
+      reserves,
+      result: lpAddress
+    }
+  } catch (err: any) {
+    console.error(`Error in getSwapPrice(): `, err)
   }
+
 }
 
 export async function getOneAssetInfo(asset: string, address: string, account: any, assetBaseInfoObj: any) {
@@ -207,8 +226,8 @@ export async function getOneAssetInfo(asset: string, address: string, account: a
     return false
   }
   const provider = window.ethereum
-  const library = getLibrary(provider)??simpleRpcProvider
+  const library = getLibrary(provider) ?? simpleRpcProvider
   const contract = new ethers.Contract(address, Erc20Abi, library)
   const balance = formatUnits(await contract.balanceOf(account), assetBaseInfoObj[asset].decimals)
-  return {balance}
+  return { balance }
 }
